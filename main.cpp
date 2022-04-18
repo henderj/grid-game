b9b02b5cd5d773ebe5c2e87f616d27626f2f404b
Josh (ubuntu) <hendershotjoshr@gmail.com>
2022-04-18 14:31:52 -0400

    restarted

--- main.cpp
+++ main.cpp
@@ -1,179 +1,107 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
-and may not be redistributed without written permission.*/
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
 
-//Using SDL, SDL_image, standard IO, and, strings
-#include <SDL.h>
-#include <SDL_image.h>
-#include <cstdio>
-#include "src/Dot.h"
-#include "src/LTimer.h"
-#include "src/config.h"
-
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-
-
-bool init()
+int main(int argc, char *argv[])
 {
-    //Initialization flag
-    bool success = true;
 
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
-        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-        success = false;
+        printf("error initializing SDL: %s\n", SDL_GetError());
     }
-    else
+    SDL_Window *win = SDL_CreateWindow("GAME",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       1000, 1000, 0);
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+
+    SDL_Renderer *rend = SDL_CreateRenderer(win, -1, render_flags);
+    SDL_Surface *temp;
+    temp = IMG_Load("capguy-walk.png");
+
+    SDL_Texture *spritesheet = SDL_CreateTextureFromSurface(rend, temp);
+    SDL_FreeSurface(temp);
+
+    SDL_Rect windowRect;
+    windowRect.w = 140;
+    windowRect.h = 200;
+    windowRect.x = (1000 - windowRect.w) / 2;
+    windowRect.y = (1000 - windowRect.h) / 2;
+
+    SDL_Rect textureRect;
+    textureRect.x = 0;
+    textureRect.y = 0;
+    SDL_QueryTexture(spritesheet, NULL, NULL, &textureRect.w, &textureRect.h);
+
+    textureRect.w /= 8;
+
+    int close = 0;
+    int speed = 10;
+
+    while (!close)
     {
-        //Set texture filtering to linear
-        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+        SDL_Event event;
+        int totalFrames = 8;
+        int delayPerFrame = 100;
+        while (SDL_PollEvent(&event))
         {
-            printf( "Warning: Linear texture filtering not enabled!" );
-        }
-
-        //Create window
-        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( gWindow == nullptr )
-        {
-            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-            success = false;
-        }
-        else
-        {
-            //Create renderer for window
-            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-            if( gRenderer == nullptr )
+            switch (event.type)
             {
-                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-                success = false;
-            }
-            else
-            {
-                //Initialize renderer color
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-                //Initialize PNG loading
-                int imgFlags = IMG_INIT_PNG;
-                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+            case SDL_QUIT:
+                close = 1;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.scancode)
                 {
-                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-                    success = false;
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    windowRect.y -= speed;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    windowRect.x -= speed;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    windowRect.y += speed;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    windowRect.x += speed;
+                    break;
+                default:
+                    break;
                 }
+
+            default:
+                break;
             }
         }
+
+        if (windowRect.x + windowRect.w > 1000)
+            windowRect.x = 1000 - windowRect.w;
+        if (windowRect.x < 0)
+            windowRect.x = 0;
+        if (windowRect.y + windowRect.h > 1000)
+            windowRect.y = 1000 - windowRect.h;
+        if (windowRect.y < 0)
+            windowRect.y = 0;
+
+        int frame = (SDL_GetTicks() / delayPerFrame) % totalFrames;
+        textureRect.x = frame * textureRect.w;
+
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, spritesheet, &textureRect, &windowRect);
+
+        SDL_RenderPresent(rend);
+        SDL_Delay(1000 / 60);
     }
 
-    return success;
-}
-
-bool loadMedia()
-{
-    //Loading success flag
-    bool success = true;
-
-    //Load dot texture
-    if( !gDotTexture.loadFromFile( "ball_blue_large.png" ) )
-    {
-        printf( "Failed to load dot texture!\n" );
-        success = false;
-    }
-
-    return success;
-}
-
-void close()
-{
-    //Free loaded images
-    gDotTexture.free();
-
-    //Destroy window
-    SDL_DestroyRenderer( gRenderer );
-    SDL_DestroyWindow( gWindow );
-    gWindow = nullptr;
-    gRenderer = nullptr;
-
-    //Quit SDL subsystems
-    IMG_Quit();
+    SDL_DestroyTexture(spritesheet);
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
     SDL_Quit();
-}
-
-int main()
-{
-    //Start up SDL and create window
-    if( !init() )
-    {
-        printf( "Failed to initialize!\n" );
-    }
-    else
-    {
-        //Load media
-        if( !loadMedia() )
-        {
-            printf( "Failed to load media!\n" );
-        }
-        else
-        {
-            //Main loop flag
-            bool quit = false;
-
-            //Event handler
-            SDL_Event e;
-
-            //The dot that will be moving around on the screen
-            Dot dot;
-
-            //Keeps track of time between steps
-            LTimer stepTimer;
-
-            //While application is running
-            while( !quit )
-            {
-                //Handle events on queue
-                while( SDL_PollEvent( &e ) != 0 )
-                {
-                    //User requests quit
-                    if( e.type == SDL_QUIT )
-                    {
-                        quit = true;
-                    }
-
-                    //Handle input for the dot
-                    dot.handleEvent( e );
-                }
-
-                //Calculate time step
-                float timeStep = stepTimer.getTicks() / 1000.f;
-
-                //Move for time step
-                dot.move( timeStep );
-
-                //Restart step timer
-                stepTimer.start();
-
-                //Clear screen
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-                SDL_RenderClear( gRenderer );
-
-                //Render dot
-                dot.render();
-
-                //Update screen
-                SDL_RenderPresent( gRenderer );
-            }
-        }
-    }
-
-    //Free resources and close SDL
-    close();
 
     return 0;
 }
