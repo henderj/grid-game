9e82b9bf8f9c7f213ff0c460c861658b46a17223
Josh (ubuntu) <hendershotjoshr@gmail.com>
2022-04-18 18:03:38 -0400

    some refactoring

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 #include "src/config.h"
 
-SDL_Rect getSpritesheetRect(const SDL_Point *pos)
+SDL_Rect GetSpriteRectFromSheet(const SDL_Point *pos)
 {
     /*
     Tilesheet information:
@@ -18,46 +18,15 @@ SDL_Rect getSpritesheetRect(const SDL_Point *pos)
     Total tiles in sheet      •  1078 tiles
     */
     SDL_Rect rect;
-    rect.w = 16;
-    rect.h = 16;
-    rect.x = pos->x * 16 + pos->x;
-    rect.y = pos->y * 16 + pos->y;
+    rect.w = TILESIZE;
+    rect.h = TILESIZE;
+    rect.x = pos->x * TILESIZE + pos->x;
+    rect.y = pos->y * TILESIZE + pos->y;
     return rect;
 }
 
-SDL_Surface *buildWorld(SDL_Surface *sprites, int w, int h)
+SDL_Surface *CreateBlankSurface(int w, int h)
 {
-    /*
-    self.tiles = np.zeros(self.size, np.int16)
-        ground_types = [TileTypes.DIRT, TileTypes.DIRT_COURSE, TileTypes.GRASS]
-        for x in range(self.size[0]):
-            for y in range(self.size[1]):
-                tile = choice(ground_types)
-                self.tiles[x][y] = tile.value
-
-        self.entities: list[Entity] = []
-        self.entities.append(Chicken(Vector2(0,0)))
-        self.entities.append(Chicken(Vector2(5,0)))
-        self.entities.append(Chicken(Vector2(-4,3)))
-
-    def load_surfaces(self, sheet: list[Surface]):
-        for x in range(self.size[0]):
-            for y in range(self.size[1]):
-                surfacepos = (x*TILESIZE, y*TILESIZE)
-                rect = Rect(surfacepos[0],surfacepos[1],TILESIZE,TILESIZE)
-                self.map_surface.blit(sheet[self.tiles[x][y]], rect)
-    */
-
-    const SDL_Point DIRT = {0, 0};
-    const SDL_Point DIRT_COURSE = {1, 0};
-    const SDL_Point GRASS = {5, 0};
-
-    const SDL_Point ground_types[3] = {DIRT,
-                                       DIRT_COURSE,
-                                       GRASS};
-
-    SDL_Point tiles[w][h];
-
     /* Create a 32-bit surface with the bytes of each pixel in R,G,B,A order,
            as expected by OpenGL for textures */
     SDL_Surface *surface;
@@ -77,15 +46,28 @@ SDL_Surface *buildWorld(SDL_Surface *sprites, int w, int h)
     amask = 0xff000000;
 #endif
 
-    surface = SDL_CreateRGBSurface(0, w * 16, h * 16, 32,
+    surface = SDL_CreateRGBSurface(0, w, h, 32,
                                    rmask, gmask, bmask, amask);
     if (surface == NULL)
     {
         SDL_Log("SDL_CreateRGBSurface() failed: %s", SDL_GetError());
         exit(1);
     }
+    return surface;
+}
 
-    SDL_Surface *map_surface = surface;
+SDL_Surface *BuildWorld(SDL_Surface *sprites, int w, int h)
+{
+
+    const SDL_Point DIRT = {0, 0};
+    const SDL_Point DIRT_COURSE = {1, 0};
+    const SDL_Point GRASS = {5, 0};
+
+    const SDL_Point ground_types[3] = {DIRT,
+                                       DIRT_COURSE,
+                                       GRASS};
+
+    SDL_Surface *map_surface = CreateBlankSurface(w * TILESIZE, h * TILESIZE);
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -96,21 +78,38 @@ SDL_Surface *buildWorld(SDL_Surface *sprites, int w, int h)
         for (int y = 0; y < h; y++)
         {
             int index = distrib(gen);
-            SDL_Point tileType = tiles[x][y] = ground_types[index];
+            SDL_Point tileType = ground_types[index];
 
             SDL_Rect destrect;
-            destrect.x = x * 16;
-            destrect.y = y * 16;
-            destrect.w = 16;
-            destrect.h = 16;
+            destrect.x = x * TILESIZE;
+            destrect.y = y * TILESIZE;
+            destrect.w = TILESIZE;
+            destrect.h = TILESIZE;
 
-            const SDL_Rect srcrect = getSpritesheetRect(&tileType);
+            const SDL_Rect srcrect = GetSpriteRectFromSheet(&tileType);
 
             SDL_BlitSurface(sprites, &srcrect, map_surface, &destrect);
         }
     }
 
     return map_surface;
+}
+
+class World
+{
+private:
+    /* data */
+public:
+    World(int w, int h);
+    ~World();
+};
+
+World::World(int w, int h)
+{
+}
+
+World::~World()
+{
 }
 
 int main(int argc, char *argv[])
@@ -133,19 +132,16 @@ int main(int argc, char *argv[])
     temp = IMG_Load("colored-transparent.png");
     SDL_Texture *spritesheet = SDL_CreateTextureFromSurface(rend, temp);
 
-    SDL_Surface *temp_map = buildWorld(temp, SCREEN_WIDTH / 16, SCREEN_HEIGHT / 16);
+    SDL_Surface *temp_map = BuildWorld(temp, 100, 100);
     SDL_Texture *map = SDL_CreateTextureFromSurface(rend, temp_map);
 
     SDL_FreeSurface(temp_map);
     SDL_FreeSurface(temp);
 
     SDL_Rect windowRect;
-    windowRect.w = SCREEN_WIDTH;
-    windowRect.h = SCREEN_HEIGHT;
+    SDL_QueryTexture(map, NULL, NULL, &windowRect.w, &windowRect.h);
     windowRect.x = 0;
     windowRect.y = 0;
-
-    // SDL_Rect textureRect = getSpritesheetRect(1, 0);
 
     int close = 0;
     int speed = 8;
@@ -189,18 +185,6 @@ int main(int argc, char *argv[])
                 break;
             }
         }
-
-        // if (windowRect.x + windowRect.w > SCREEN_WIDTH)
-        //     windowRect.x = 1000 - windowRect.w;
-        // if (windowRect.x < 0)
-        //     windowRect.x = 0;
-        // if (windowRect.y + windowRect.h > SCREEN_HEIGHT)
-        //     windowRect.y = 1000 - windowRect.h;
-        // if (windowRect.y < 0)
-        //     windowRect.y = 0;
-
-        // int frame = (SDL_GetTicks() / delayPerFrame) % totalFrames;
-        // textureRect.x = frame * textureRect.w;
 
         SDL_SetRenderDrawColor(rend, 71, 45, 60, 255);
         SDL_RenderClear(rend);
--- src/config.h
+++ src/config.h
@@ -6,3 +6,4 @@
 // Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int TILESIZE = 16;
